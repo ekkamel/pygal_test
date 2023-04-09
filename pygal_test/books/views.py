2f526e501b44896017879fc8e84c00cbe63f93db
ekkamel <ekkamel@gmail.com>
2023-04-09 22:35:59 +0200

    Added files

---
+++ pygal_test/books/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Books
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+import pygal    
+
+# Create your views here.
+def BookGraph(request):
+    data = []
+    context = {}
+    books_list = Books.objects.all()
+    context['books_list'] = books_list
+    
+    for book in Books.objects.all(): 
+        data.append(book.price)
+    
+    # make the graph
+    book_chart = pygal.Bar()
+    book_chart.add('Books', data)
+    book_chart.render_to_file('books/static/book_chart.svg')
+    
+    
+    return render(request, 'books/home.html', context)
