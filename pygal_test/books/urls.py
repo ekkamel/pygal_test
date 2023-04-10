728264a31efd1c68bfcebc5418b9c564db0abb35
ekkamel <ekkamel@gmail.com>
2023-04-10 13:24:08 +0200

    Added static settings

--- pygal_test/books/urls.py
+++ pygal_test/books/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path 
+from django.conf import settings
 from . import views
+from django.conf.urls.static import static
 
 urlpatterns = [
     path(route='', view=views.BookGraph, name='BookGraph')
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- pygal_test/books/views.py
+++ pygal_test/books/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Books
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
-import pygal    
+from django.conf.urls.static import static
+import pygal   
 
 # Create your views here.
 def BookGraph(request):
@@ -17,7 +18,7 @@ def BookGraph(request):
     # make the graph
     book_chart = pygal.Bar()
     book_chart.add('Books', data)
-    book_chart.render_to_file('books/static/book_chart.svg')
+    book_chart.render_to_file('static/book_chart.svg')
     
     
     return render(request, 'books/home.html', context)
